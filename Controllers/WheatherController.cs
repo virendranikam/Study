acc05a8a4b30fa0d070c4324d2e775fcafb0b56f
Virendra Nikam <virendra.nikam@outlook.com>
2025-03-22 12:52:25 +0530

    Add project files.

---
+++ Controllers/WheatherController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Study.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WheatherController : Controller
+    {  
+        public ViewResult Index()
+        {
+            return View() ;
+        }
+        [HttpGet(Name ="GetRecords")]
+        public List<string> GetRecords(int id=10)
+        {
+            List<string> records = new List<string>();
+            try
+            {
+                // Create Dummat String list  
+                for(int i = 0; i < id; i++)
+                {
+                    records.Add(i.ToString());
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+            }
+            return records;
+        }
+    }
+}
